dd7e6038ef8f6f238caec2f386690b35aaec7cad
kalakrumanov <viktorbeat@yandex.com>
2016-01-11 01:27:25 +0100

    Varias mejoras:
    Usuario elige tiempo, y le da a "START" y sale Toast cada X segundos que el marcó usuario.

--- app/src/main/java/com/examples/Main.java
+++ app/src/main/java/com/examples/Main.java
@@ -6,123 +6,203 @@ import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Main extends Activity implements OnClickListener
+public class Main extends Activity
 {
-	private static final String tag = "Main";
-	private MalibuCountDownTimer countDownTimer;
-	private long timeElapsed;
-	private boolean timerHasStarted = false;
-	private Button startB;
-	private TextView text;
-	private TextView timeElapsedView;
+    private static final String tag = "Main";
+    private MalibuCountDownTimer countDownTimer;
+    private long timeElapsed;
+    private boolean timerHasStarted = false;
+    private Button startB;
+    private TextView text;
+    private TextView timeElapsedView;
 
-	private final long startTime = 50000;
-	private final long interval = 1000;
+    private long startTime = 50000;
+    private long interval = 1000;
 
-	int x=0;
+    private Spinner sCuantosSegundos;
+    private Button btnSubmit;
 
-	GPSTracker gps;
+    int x=0;
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		startB = (Button) this.findViewById(R.id.button);
-		startB.setOnClickListener(this);
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-		text = (TextView) this.findViewById(R.id.timer);
-		timeElapsedView = (TextView) this.findViewById(R.id.timeElapsed);
-		countDownTimer = new MalibuCountDownTimer(startTime, interval);
-		text.setText(text.getText() + String.valueOf(startTime));
-	}
+        /** ELEGIR SEGUNDOS **/
 
-	@Override
-	public void onClick(View v)
-	{
-		if (!timerHasStarted)
-		{
-			countDownTimer.start();
-			timerHasStarted = true;
-			startB.setText("Start");
-		}
-		else
-		{
+        //addItemsOnSpinner2();
+        addListenerOnButton();
+        addListenerOnSpinnerItemSelection();
 
-			countDownTimer.cancel();
-			timerHasStarted = false;
-			startB.setText("RESET");
-		}
-	}
 
-	// CountDownTimer class
-	public class MalibuCountDownTimer extends CountDownTimer
-	{
+        /** CONTADOR **/
 
-		public MalibuCountDownTimer(long startTime, long interval)
-		{
-			super(startTime, interval);
-		}
+        startB = (Button) this.findViewById(R.id.empezarContador);
+        startB.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!timerHasStarted)
+                {
+                    countDownTimer.start();
+                    timerHasStarted = true;
+                    startB.setText("Start");
+                }
+                else
+                {
 
-		@Override
-		public void onFinish()
-		{
-			text.setText("Time's up!");
-			timeElapsedView.setText("Time Elapsed: " + String.valueOf(startTime));
-		}
+                    countDownTimer.cancel();
+                    timerHasStarted = false;
+                    startB.setText("RESET");
+                }
+            }
+        });
 
-		@Override
-		public void onTick(long millisUntilFinished)
-		{
-			x++;
-			if (x==5) {
+
+        text = (TextView) this.findViewById(R.id.timer);
+        timeElapsedView = (TextView) this.findViewById(R.id.timeElapsed);
+        countDownTimer = new MalibuCountDownTimer(startTime, interval);
+        text.setText(text.getText() + String.valueOf(startTime));
+
+
+
+    }
+
+    private void addListenerOnSpinnerItemSelection() {
+        // Salta al elegir una opcion de "eligeCuantosSegundos"
+        sCuantosSegundos = (Spinner) findViewById(R.id.eligeCuantosSegundos);
+        sCuantosSegundos.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(parent.getContext(),
+                        "OnItemSelectedListener : " + parent.getItemAtPosition(position).toString(),
+                        Toast.LENGTH_SHORT).show();
+
+
+                if (sCuantosSegundos.getSelectedItemPosition() == 0) {
+                    Toast.makeText(getApplicationContext(), "1 segundo",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+                long posicion = sCuantosSegundos.getSelectedItemPosition();
+
+                Log.i("TAG", "Posicion: " + sCuantosSegundos.getSelectedItemPosition());
+
+                interval = (sCuantosSegundos.getSelectedItemPosition() + 1) * 1000;
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        //startTime = sCuantosSegundos.getSelectedItemId();
+
+
+
+
+    }
+
+    private void addListenerOnButton() {
+
+
+
+        // Cuando hago click en "Guardar Cambios"
+
+        sCuantosSegundos = (Spinner) findViewById(R.id.eligeCuantosSegundos);
+        //spinner2 = (Spinner) findViewById(R.id.spinner2);
+        btnSubmit = (Button) findViewById(R.id.verSegundosElegidos);
+
+        btnSubmit.setEnabled(false);
+
+        btnSubmit.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(),
+                        "OnClickListener : " +
+                                "\nSpinner 1 : " + String.valueOf(sCuantosSegundos.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+
+    /** Ejemplo de "Meter items en vivo a SPINNER **/
+
+    /*
+
+    private void addItemsOnSpinner2() {
+
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        List<String> list = new ArrayList<String>();
+        list.add("list 1");
+        list.add("list 2");
+        list.add("list 3");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(dataAdapter);
+
+    }
+
+     */
+
+
+
+    // CountDownTimer class
+    public class MalibuCountDownTimer extends CountDownTimer
+    {
+
+        public MalibuCountDownTimer(long startTime, long interval)
+        {
+            super(startTime, interval);
+        }
+
+        @Override
+        public void onFinish()
+        {
+            text.setText("Time's up!");
+            timeElapsedView.setText("Time Elapsed: " + String.valueOf(startTime));
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished)
+        {
+            x++;
+            if (x==(interval) / 1000) {
 
 				/* Empieza GPS */
 
-				GPSTracker servicio = new GPSTracker(getApplicationContext());
+                GPSTracker servicio = new GPSTracker(getApplicationContext());
+                servicio.getLocation();
 
-				servicio.getLocation();
-
-				Toast.makeText(getApplicationContext(), "Coords: "+servicio.getLatitud()+","+servicio.getLongitud(),
-						Toast.LENGTH_LONG).show();
-				Log.i("TAG", "Coords: " + servicio.getLatitud() + "," + servicio.getLongitud());
-
-
-				/*
-
-				gps = new GPSTracker(Main.this);
-
-				if(gps.canGetLocation()) {
-					double latitude = gps.getLatitude();
-					double longitude = gps.getLongitude();
-
-					Toast.makeText(
-							getApplicationContext(),
-							"Tu situación GPS es \nLat: " + latitude + "\nLong: "
-									+ longitude, Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Cada " + x + " segundos actualizo esto" +
+                        "\nCoords: "+servicio.getLatitud()+","+servicio.getLongitud(),
+                        Toast.LENGTH_SHORT).show();
+                Log.i("TAG", "Coords: " + servicio.getLatitud() + "," + servicio.getLongitud());
 
 
+                //Toast.makeText(getApplicationContext(), "Cada 5 segundos veo GPS", Toast.LENGTH_LONG).show();
 
-				} else {
-					gps.showSettingsAlert();
-				}
-
-				*/
-
-				/* FIN GPS */
-
-				//Toast.makeText(getApplicationContext(), "Cada 5 segundos veo GPS", Toast.LENGTH_LONG).show();
-
-				x=0;
-			}
-			text.setText("Time remain:" + millisUntilFinished);
-			timeElapsed = startTime - millisUntilFinished;
-			timeElapsedView.setText("Time Elapsed: " + String.valueOf(timeElapsed));
-		}
-	}
+                x=0;
+            }
+            text.setText("Time remain:" + millisUntilFinished);
+            timeElapsed = startTime - millisUntilFinished;
+            timeElapsedView.setText("Time Elapsed: " + String.valueOf(timeElapsed));
+        }
+    }
 }
